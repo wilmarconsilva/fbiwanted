d0a2d103b6e00fcb561708340ca4bcc38e971131
Wilson Marcon Da Silva <111930708+wilmac2002@users.noreply.github.com>
2022-10-10 17:43:40 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
-function get_procurados()
+function req_json (page)
 {
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", 'https://api.fbi.gov/wanted/v1/list', false);
+    xhttp.open("GET", 'https://api.fbi.gov/@wanted?pageSize=50&page='+page, false);
     xhttp.send();
     var retorno = JSON.parse(xhttp.responseText);
-    var procurados = retorno.items;
+
+    return retorno.items;
+}
+
+function get_procurados() {
+
+    var page = 1;
+
+    var procurados = req_json(page);
+    
+    for (var i in procurados)
+    {
+        if(procurados[i].reward_text == null || procurados[i].sex == null)
+        {
+            procurados.splice(i);
+        }
+    }
+
+    if( (Object.keys(procurados).length) < 16 )
+    {
+        page+=1;
+        procurados.concat(req_json(page));
+    }
 
     return procurados;
+
 }
 
 const procurados = get_procurados();
+
 console.log(procurados);
 
-var limite1 = 5;
-var limite2 = 10;
-var limite3 = 15;
-var limite4 = 20;
-
-console.log(procurados.lenght)
+quantidade = (Object.keys(procurados).length) / 4;
+var limite1 = quantidade;
+var limite2 = quantidade * 2;
+var limite3 = quantidade * 3;
+var limite4 = quantidade * 4;
 
 var col1 = document.getElementById('col1');
 var col2 = document.getElementById('col2');
@@ -29,172 +52,177 @@ col2.innerHTML = '';
 col3.innerHTML = '';
 col4.innerHTML = '';
 
-for (var i=0; i<limite1; i++)
-{
-    var card = document.createElement("div");
-    card.classList.add("card");
-    
-    //procurado
-    var titulo = document.createElement("h4");
-    titulo.classList.add("text-danger");
-    titulo.classList.add("text-center");
-    var texto_titulo = document.createTextNode("Procurado");
+for (var i = 0; i < limite1; i++) {
 
-    titulo.appendChild(texto_titulo);
-    card.appendChild(titulo);
+        var card = document.createElement("div");
+        card.classList.add("card");
+        card.style = 'width: 18rem;';
 
-    //img
-    var link_img = document.createElement("a");
-    link_img.href = "#topo";
+        //procurado
+        var titulo = document.createElement("h4");
+        titulo.classList.add("text-danger");
+        titulo.classList.add("text-center");
+        var texto_titulo = document.createTextNode("Procurado");
 
-    var img = document.createElement("img");
-    img.src = procurados[i].images[0].thumb;
+        titulo.appendChild(texto_titulo);
+        card.appendChild(titulo);
 
-    link_img.appendChild(img);
-    card.appendChild(link_img);
+        //img
+        var link_img = document.createElement("a");
+        link_img.href = "#topo";
 
-    //card body
-    var link_nome = document.createElement("a");
-    link_nome.href = "#topo";
+        var img = document.createElement("img");
+        img.src = procurados[i].images[0].thumb;
 
-    var card_body = document.createElement("div");
+        link_img.appendChild(img);
+        card.appendChild(link_img);
 
-    var nome = document.createElement("h5");
-    nome.classList.add("text-center");
-    var texto_nome = document.createTextNode(procurados[i].title);
+        //card body
+        var link_nome = document.createElement("a");
+        link_nome.href = "#topo";
 
-    link_nome.appendChild(nome);
-    nome.appendChild(texto_nome);
-    card_body.appendChild(link_nome);
-    card.appendChild(card_body);
+        var card_body = document.createElement("div");
 
-    col1.appendChild(card);
+        var nome = document.createElement("h5");
+        nome.classList.add("text-center");
+        var texto_nome = document.createTextNode(procurados[i].title);
+
+        //link_nome.appendChild(nome);
+        nome.appendChild(texto_nome);
+        card_body.appendChild(nome);
+        card.appendChild(card_body);
+
+        col1.appendChild(card);
+
 }
 
-for (var i=limite1; i<limite2; i++)
-{
-    var card = document.createElement("div");
-    card.classList.add("card");
-    
-    //procurado
-    var titulo = document.createElement("h4");
-    titulo.classList.add("text-danger");
-    titulo.classList.add("text-center");
-    var texto_titulo = document.createTextNode("Procurado");
+for (var i = limite1; i < limite2; i++) {
 
-    titulo.appendChild(texto_titulo);
-    card.appendChild(titulo);
+        var card = document.createElement("div");
+        card.classList.add("card");
+        card.style = 'width: 18rem;';
 
-    //img
-    var link_img = document.createElement("a");
-    link_img.href = "#topo";
+        //procurado
+        var titulo = document.createElement("h4");
+        titulo.classList.add("text-danger");
+        titulo.classList.add("text-center");
+        var texto_titulo = document.createTextNode("Procurado");
 
-    var img = document.createElement("img");
-    img.src = procurados[i].images[0].thumb;
+        titulo.appendChild(texto_titulo);
+        card.appendChild(titulo);
 
-    link_img.appendChild(img);
-    card.appendChild(link_img);
+        //img
+        var link_img = document.createElement("a");
+        link_img.href = "#topo";
 
-    //card body
-    var link_nome = document.createElement("a");
-    link_nome.href = "#topo";
+        var img = document.createElement("img");
+        img.src = procurados[i].images[0].thumb;
 
-    var card_body = document.createElement("div");
+        link_img.appendChild(img);
+        card.appendChild(link_img);
 
-    var nome = document.createElement("h5");
-    nome.classList.add("text-center");
-    var texto_nome = document.createTextNode(procurados[i].title);
+        //card body
+        var link_nome = document.createElement("a");
+        link_nome.href = "#topo";
 
-    link_nome.appendChild(nome);
-    nome.appendChild(texto_nome);
-    card_body.appendChild(link_nome);
-    card.appendChild(card_body);
+        var card_body = document.createElement("div");
 
-    col2.appendChild(card);
+        var nome = document.createElement("h5");
+        nome.classList.add("text-center");
+        var texto_nome = document.createTextNode(procurados[i].title);
+
+        //link_nome.appendChild(nome);
+        nome.appendChild(texto_nome);
+        card_body.appendChild(nome);
+        card.appendChild(card_body);
+
+        col2.appendChild(card);
 }
 
-for (var i=limite2; i<limite3; i++)
-{
-    var card = document.createElement("div");
-    card.classList.add("card");
-    
-    //procurado
-    var titulo = document.createElement("h4");
-    titulo.classList.add("text-danger");
-    titulo.classList.add("text-center");
-    var texto_titulo = document.createTextNode("Procurado");
+for (var i = limite2; i < limite3; i++) {
 
-    titulo.appendChild(texto_titulo);
-    card.appendChild(titulo);
+        var card = document.createElement("div");
+        card.classList.add("card");
+        card.style = 'width: 18rem;';
 
-    //img
-    var link_img = document.createElement("a");
-    link_img.href = "#topo";
+        //procurado
+        var titulo = document.createElement("h4");
+        titulo.classList.add("text-danger");
+        titulo.classList.add("text-center");
+        var texto_titulo = document.createTextNode("Procurado");
 
-    var img = document.createElement("img");
-    img.src = procurados[i].images[0].thumb;
+        titulo.appendChild(texto_titulo);
+        card.appendChild(titulo);
 
-    link_img.appendChild(img);
-    card.appendChild(link_img);
+        //img
+        var link_img = document.createElement("a");
+        link_img.href = "#topo";
 
-    //card body
-    var link_nome = document.createElement("a");
-    link_nome.href = "#topo";
+        var img = document.createElement("img");
+        img.src = procurados[i].images[0].thumb;
 
-    var card_body = document.createElement("div");
+        link_img.appendChild(img);
+        card.appendChild(link_img);
 
-    var nome = document.createElement("h5");
-    nome.classList.add("text-center");
-    var texto_nome = document.createTextNode(procurados[i].title);
+        //card body
+        var link_nome = document.createElement("a");
+        link_nome.href = "#topo";
 
-    link_nome.appendChild(nome);
-    nome.appendChild(texto_nome);
-    card_body.appendChild(link_nome);
-    card.appendChild(card_body);
+        var card_body = document.createElement("div");
 
-    col3.appendChild(card);
+        var nome = document.createElement("h5");
+        nome.classList.add("text-center");
+        var texto_nome = document.createTextNode(procurados[i].title);
+
+        //link_nome.appendChild(nome);
+        nome.appendChild(texto_nome);
+        card_body.appendChild(nome);
+        card.appendChild(card_body);
+
+        col3.appendChild(card);
 }
 
-for (var i=limite3; i<limite4; i++)
-{
-    var card = document.createElement("div");
-    card.classList.add("card");
-    
-    //procurado
-    var titulo = document.createElement("h4");
-    titulo.classList.add("text-danger");
-    titulo.classList.add("text-center");
-    var texto_titulo = document.createTextNode("Procurado");
+for (var i = limite3; i < limite4; i++) {
 
-    titulo.appendChild(texto_titulo);
-    card.appendChild(titulo);
+        var card = document.createElement("div");
+        card.classList.add("card");
+        card.style = 'width: 18rem;';
 
-    //img
-    var link_img = document.createElement("a");
-    link_img.href = "#topo";
+        //procurado
+        var titulo = document.createElement("h4");
+        titulo.classList.add("text-danger");
+        titulo.classList.add("text-center");
+        var texto_titulo = document.createTextNode("Procurado");
 
-    var img = document.createElement("img");
-    img.src = procurados[i].images[0].thumb;
+        titulo.appendChild(texto_titulo);
+        card.appendChild(titulo);
 
-    link_img.appendChild(img);
-    card.appendChild(link_img);
+        //img
+        var link_img = document.createElement("a");
+        link_img.href = "#topo";
 
-    //card body
-    var link_nome = document.createElement("a");
-    link_nome.href = "#topo";
+        var img = document.createElement("img");
+        img.src = procurados[i].images[0].thumb;
 
-    var card_body = document.createElement("div");
+        link_img.appendChild(img);
+        card.appendChild(link_img);
 
-    var nome = document.createElement("h5");
-    nome.classList.add("text-center");
-    var texto_nome = document.createTextNode(procurados[i].title);
+        //card body
+        //var link_nome = document.createElement("a");
+        //link_nome.href = "#topo";
 
-    link_nome.appendChild(nome);
-    nome.appendChild(texto_nome);
-    card_body.appendChild(link_nome);
-    card.appendChild(card_body);
+        var card_body = document.createElement("div");
 
-    col4.appendChild(card);
+        var nome = document.createElement("h5");
+        nome.classList.add("text-center");
+        var texto_nome = document.createTextNode(procurados[i].title);
+
+        //link_nome.appendChild(nome);
+        nome.appendChild(texto_nome);
+        card_body.appendChild(nome);
+        card.appendChild(card_body);
+
+        col4.appendChild(card);
 }
 
 
