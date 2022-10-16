2e551f009d5a5d8c7b3fdb66dea1ab9f085e810d
Wilson Marcon Da Silva <111930708+wilmac2002@users.noreply.github.com>
2022-10-15 23:58:50 -0300

    v6

--- app.js
+++ app.js
@@ -5,77 +5,80 @@ function req_json_wanted (page)
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", 'https://api.fbi.gov/@wanted?pageSize=50&page='+page, false);
     xhttp.send();
-    var retorno = JSON.parse(xhttp.responseText);
-    console.log(retorno);
-    var procurados = retorno.items;
+    const retorno = JSON.parse(xhttp.responseText);
 
-    //mantém somente criminosos no objeto
-     for (var i in procurados)
-     {
-         if(procurados[i].reward_text == null || procurados[i].sex == null)
-         {
-             procurados.splice(i);
-         }
-     }
-
-    return procurados;
+    return retorno.items;
 }
 
 function get_procurados() {
 
     var page = 1;
 
-    var procurados = req_json_wanted(page);
+    var aux = req_json_wanted(page);
+    var procurados = aux;
 
-    //requisita uma nova página caso não alcance 12 criminosos no filtro
-    while( (Object.keys(procurados).length) < 12 )
+    //requisita uma nova página até que a requisição não tenha mais dados
+    while (Object.keys(aux).length != 0)
     {
         page+=1;
 
-        var aux = req_json_wanted(page);
+        //mantém somente criminosos no objeto
+        for (var i in procurados)
+        {
+            if(procurados[i].reward_text == null || procurados[i].sex == null)
+            {
+                procurados.splice(i);
+            }
+        }
+
+        aux = req_json_wanted(page);
         procurados = procurados.concat(aux);
-    }
+     }
 
-    return procurados;
+     console.log(procurados);
+     return procurados;
 
-}
+ }
 
-function req_json_wanted_person (uid)
+function req_json_wanted_person (id)
 {
-    console.log(uid);
-
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", 'https://api.fbi.gov/@wanted-person/'+uid, false);
+    xhttp.open("GET", 'https://api.fbi.gov/@wanted-person/'+id, false);
     xhttp.send();
-    var retorno = JSON.parse(xhttp.responseText);
-    const procurado = retorno.items;
+    const procurado = JSON.parse(xhttp.responseText);
 
-    console.log(procurado);
+    return procurado;
 }
 
-function get_procurado (id)
+function sobre (id)
 {
     const procurado = req_json_wanted_person(id);
-    console.log(procurado);
 
     var img = document.getElementById("img-card-sobre");
-    img.src = procurado.images[0].original;
+    img.src = procurado.images[0].thumb;
 
     document.getElementById("h1-nome-procurado").innerHTML = procurado.title;
     document.getElementById("descricao").innerHTML = procurado.description;
-    document.getElementById("h1-recompensa").innerHTML = procurado.reward_text;
+    document.getElementById("h1-recompensa").innerHTML = get_reward(procurado);
 }
+
+function get_reward(procurado)
+{
+    const result = 'U$ ' + procurado.reward_text.replace(/([^\d])+/gim, '');
+
+    return result;
+}
+
+
 
 //main
 
 const procurados = get_procurados();
 
-console.log(procurados);
+var row_cards = document.getElementById("row-card");
 
-var col_card = document.getElementById('col1');
-
-//zerar coluna
-col_card.innerHTML = '';
+//zerar row
+row_cards.innerHTML = '';
 
 //cria os cards
 for (var i in procurados) {
@@ -83,26 +86,29 @@ for (var i in procurados) {
         var card = document.createElement("div");
         card.classList.add("card");
         card.style = 'width: 18rem;';
+        card.id = 'card-index';
 
         //procurado
         var titulo = document.createElement("h4");
         titulo.classList.add("text-danger");
         titulo.classList.add("text-center");
-        var texto_titulo = document.createTextNode("Procurado");
+        var texto_titulo = document.createTextNode("procurado");
 
         titulo.appendChild(texto_titulo);
         card.appendChild(titulo);
 
         //img
         var link_img = document.createElement("a");
-        link_img.href = './sobre.html';
-        link_img.id = procurados.uid;
+        link_img.href = '#jumbo-sobre';
+        link_img.id = procurados[i].uid;
         link_img.onclick = function (e)
         {
-            get_procurado(this.id);
+            sobre(this.id);
+            document.getElementById("jumbo-sobre").style.display = 'block';
         }
 
         var img = document.createElement("img");
+        img.id = 'img-card-index';
         img.src = procurados[i].images[0].original;
 
         link_img.appendChild(img);
@@ -120,10 +126,6 @@ for (var i in procurados) {
         card_body.appendChild(nome);
         card.appendChild(card_body);
 
-        col_card.appendChild(card);
+        row_cards.appendChild(card);
 
 }
-
-teste = '3f5d03cb681c454f8cc324c3303a579d';
-get_procurado(teste);
-
